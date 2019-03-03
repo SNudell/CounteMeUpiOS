6547712432dcc5beea7f4b0842a0b2edbf1f1e96
Gustav Schmid <sch.gus@gmx.de>
2019-03-03 18:20:24 +0100

    Initial Commit

---
+++ CountMeUpTests/CountMeUpTests.swift
@@ -0,0 +1,34 @@
+//
+//  CountMeUpTests.swift
+//  CountMeUpTests
+//
+//  Created by Gustav Schmid on 03.03.19.
+//  Copyright © 2019 Gustav Schmid. All rights reserved.
+//
+
+import XCTest
+@testable import CountMeUp
+
+class CountMeUpTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
